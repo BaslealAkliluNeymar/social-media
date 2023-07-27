56fab211c88b3f80edd3cb6d77729d0a58b337e9
BaslealAkliluNeymar <baslealaklilu@gmail.com>
2023-07-27 21:59:14 +0300

    second-commit

--- src/components/Post/Post.jsx
+++ src/components/Post/Post.jsx
@@ -15,20 +15,20 @@ function Post() {
                 </div>
             </div>
             <div className="postCenter">
-                {/* <img src="public\assets\post\1.jpeg" alt="" className="postCenterImg" /> */}
+                 <span className="postText">
+                    Hey! Its my first post!
+                 </span>
+                <img src="public\assets\post\1.jpeg" alt="" className="postCenterImg" />
             </div>
             <div className="postBottom">
-                <ul className="postIcons">
-                    <li className="postIcon">
-                            {/* <img src="public\assets\like.png" alt="" className="icon" /> */}
-                    </li>
-                    <li className="postIcon">
-                        {/* <img src="public\assets\heart.png" alt="" className="icon" /> */}
-                    </li>
-                    <li className="postIcon">
-                        {/* <img src="public\assets\gift.png" alt="" className="icon" /> */}
-                    </li>
-                </ul>
+                <div className="postBottomLeft">
+                    <img src="public\assets\like.png" alt="" className="likeIcon" />
+                    <img src="public\assets\heart.png" alt="" className="likeIcon" />
+                    <span className="postLikeCounter">32 people liked it.</span>
+                </div>
+                <div className="postBottomRight">
+                    <span className="postCommentText">9 comments</span>
+                </div>
             </div>
         </div>
     </div>
--- src/components/rightbar/RightBar.jsx
+++ src/components/rightbar/RightBar.jsx
@@ -2,7 +2,17 @@ import './rightbar.css'
 
 function RightBar() {
   return (
-    <div className='rightbar'>RightBar</div>
+    <div className='rightbar'>
+      <div className="rightbarWrapper">
+        <div className="birthdayContainer">
+          <img src="/assets/gift.png" alt="" className="birthdayImg" />
+          <span className="birthdayText">
+            <b>Pola foster</b> and <b>3 other friends</b> have a birthday today
+          </span>
+        </div>
+        <img src="/assets/ad.png" alt="" className="rightbarAd" />
+      </div>
+    </div>
   )
 }
 
